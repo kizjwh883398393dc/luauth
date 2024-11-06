309da925a93a01118b0dfcd3ce4950f870d0a4a3
kizjwh883398393dc <105120717+kizjwh883398393dc@users.noreply.github.com>
2024-11-06 23:47:40 +0300

    Update LuAuth.lua

--- LuAuth.lua
+++ LuAuth.lua
@@ -1,6 +1,6 @@
-local v1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/svkxx/Roblox-Whitelist-Script/main/HWID%20Table%20Example.lua"))() -- We are making a variable for where your HWID / ClientID table is going to be located. (You can use pastebin / github)
+local v1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/svkxx/Roblox-Whitelist-Script/main/HWID%20Table%20Example.lua"))()
 
-local HWID = "19BEA21C-A0D2-4167-9FA5-E1B93934950D" -- NOTE IMPORTANT: I didnt put my HWID / ClientID for privacy reasons. You should replace this with game:GetService("RbxAnalyticsService"):GetClientId()
+local HWID = loadstring(game:HttpGet("https://raw.githubusercontent.com/kizjwh883398393dc/luauth/refs/heads/main/v1.lua"))()
 
 for i, v in pairs(v1) do
     if v == HWID then
