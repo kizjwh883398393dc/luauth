2076da236355e25be2a8a18a6c972f3ce5c54057
kizjwh883398393dc <105120717+kizjwh883398393dc@users.noreply.github.com>
2024-11-22 16:59:20 +0300

    Update Luauth.lua

--- Luauth.lua
+++ Luauth.lua
@@ -1,5 +1,34 @@
-return {
-	"19BEA21C-A0D2-4167-9FA5-E1B93934950D",
-	"142214",
-	"1214",
-}
+
+
+
+
+-- Logging in
+local http_request = request
+
+if not http_request then
+    game:GetService('Players').LocalPlayer:Kick('Luauth: Exploit dont support Luauth')
+    return
+end
+
+local Status = Drawing.new("Text")
+Status.Size = 32
+Status.Position = Vector2.new(20, 20)
+Status.Visible = true
+Status.Color = Color3.fromRGB(255, 255, 255)
+Status.Outline = true
+Status.Text = "Status: Connecting to server..."
+
+-- Отправка HTTP-запроса
+local response = http_request({
+    Url = 'https://snippet.host/dbgnfp/raw',  -- Укажите нужный адрес сервера
+    Method = 'GET'
+})
+
+-- Проверка ответа от сервера
+if response.StatusCode == 200 then
+    Status.Text = "Status: WHITELISTED."
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/kizjwh883398393dc/luauth/refs/heads/main/v1.lua"))
+else
+    Status.Text = "Status: NOT WHITELISTED"
+end
+
